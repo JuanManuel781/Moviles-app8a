fac548f74edc0894a2d420b5c31ebcb78f42c063
JuanManuel781 <juanmanuelguaquespud@gmail.com>
2021-04-28 19:33:27 -0500

    My basic calc

---
+++ My basic calc/app/src/main/java/com/example/entrenamiento/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.entrenamiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1, et2;
+    private  Spinner sp1;
+    private TextView tv3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.et1=(EditText)findViewById(R.id.editText1);
+        this.et2=(EditText)findViewById(R.id.editText2);
+        this.sp1=(Spinner)findViewById(R.id.sp1);
+        this.tv3=(TextView)findViewById(R.id.textView5);
+
+        /* Crear un vector */
+
+        String[] opciones={"Sumar","Restar","Multiplicar", "Dividir"};
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>
+                (this,android.R.layout.simple_spinner_item, opciones);
+        sp1.setAdapter(adapter);
+    }
+
+    public void operaciones_ma(View view){
+
+
+       int num1, num2;
+        int sum, resta,mult,divi;
+        String select;
+
+        num1= Integer.parseInt(this.et1.getText().toString());
+        num2= Integer.parseInt(this.et2.getText().toString());
+
+      select= sp1.getSelectedItem().toString();
+
+      if(select.equals("Sumar")){
+            sum= num1+num2;
+            tv3.setText(""+sum);
+        }
+        else {
+            if (select.equals("Restar")){
+                resta= num1-num2;
+                tv3.setText(""+resta);
+            }else {
+                if(select.equals("Multiplicar")){
+                    mult=num1*num2;
+                    tv3.setText(""+mult);
+                }else{
+                    if (select.equals("Dividir")){
+                        divi=num1/num2;
+                        tv3.setText(""+divi);
+                    }
+                }
+            }
+        }
+    }
+}
